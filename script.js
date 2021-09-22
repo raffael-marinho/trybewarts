47a4700460328ae80b11db781f85c8f9c17cf750
alt-art <pedromendescraft@gmail.com>
2021-09-22 16:44:31 -0300

    Complete requiste 3

---
+++ script.js
@@ -0,0 +1,12 @@
+const loginButton = document.querySelector('#login-button');
+const inputPassword = document.querySelector('#pass-input');
+const inputEmail = document.querySelector('#email-input');
+
+loginButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (inputEmail.value != "" || inputPassword.value != "") {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+})
