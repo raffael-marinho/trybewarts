813908c516addd8376ff1422f4b122f29ff6deb0
alt-art <pedromendescraft@gmail.com>
2021-09-22 23:51:42 -0300

    Complete requisite 21

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const loginButton = document.querySelector('#login-button');
-const inputPassword = document.querySelector('#pass-input');
-const inputEmail = document.querySelector('#email-input');
+const loginInputPassword = document.querySelector('#pass-input');
+const loginInputEmail = document.querySelector('#email-input');
 
 loginButton.addEventListener('click', (event) => {
   event.preventDefault();
-  if (inputEmail.value !== '' || inputPassword.value !== '') {
+  if (loginInputEmail.value !== '' || loginInputPassword.value !== '') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
@@ -27,10 +27,47 @@ checkbox.addEventListener('click', toggleButton);
 const counter = document.querySelector('#counter');
 const textarea = document.querySelector('#textarea');
 
-showCounter();
-
 function showCounter() {
-  counter.innerText = 500 - textarea.value.length
+  counter.innerText = 500 - textarea.value.length;
 }
 
-textarea.addEventListener("input", showCounter);
+showCounter();
+
+textarea.addEventListener('input', showCounter);
+
+const nameComplete = document.querySelector('#name-complete');
+const email = document.querySelector('#email');
+const house = document.querySelector('#home');
+const family = document.querySelector('#family');
+const learn = document.querySelector('#learn');
+const evaluation = document.querySelector('#evaluation');
+const obserb = document.querySelector('#obserb');
+
+const inputFirstName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const inputHouse = document.querySelector('#house');
+const inputFamily = document.querySelectorAll('.family-input');
+const inputLearn = document.querySelectorAll('.learn-input');
+const inputEval = document.querySelectorAll('.eval-input');
+
+function getValues(elements) {
+  const values = [];
+  elements.forEach((elem) => {
+    if (elem.checked) {
+      values.push(elem.value);
+    }
+  });
+  return values.join(', ');
+}
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  nameComplete.innerText = `Nome: ${inputFirstName.value} ${inputLastName.value}`;
+  email.innerText = `Email: ${inputEmail.value}`;
+  house.innerText = `Casa: ${inputHouse.value}`;
+  family.innerText = `Família: ${getValues(inputFamily)}`;
+  learn.innerText = `Matérias: ${getValues(inputLearn)}`;
+  evaluation.innerText = `Avaliação: ${getValues(inputEval)}`;
+  obserb.innerText = `Observações: ${textarea.value}`;
+});
