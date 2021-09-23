3f0285a77e864dd44151f48d05ca3b62784f4050
raffael marinho <silvamarinho10@gmail.com>
2021-09-22 21:03:24 -0300

    requesito 18 feito

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const loginButton = document.querySelector('#login-button');
-const inputPassword = document.querySelector('#pass-input');
-const inputEmail = document.querySelector('#email-input');
+const loginButton = document.querySelector("#login-button");
+const inputPassword = document.querySelector("#pass-input");
+const inputEmail = document.querySelector("#email-input");
 
-loginButton.addEventListener('click', (event) => {
+loginButton.addEventListener("click", (event) => {
   event.preventDefault();
-  if (inputEmail.value !== '' || inputPassword.value !== '') {
-    alert('Olá, Tryber!');
+  if (inputEmail.value !== "" || inputPassword.value !== "") {
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
 });
+
+const funçaoCheckbox = document.querySelector("#label-infos");
+const funçaoDoBotao = document.querySelector("#submit-btn");
+function botao() {
+  if (funçaoDoBotao.disabled === true) {
+    funçaoDoBotao.disabled = false;
+  } else {
+    funçaoDoBotao.disabled = true;
+  }
+}
+
+funçaoCheckbox.addEventListener("click", botao);
